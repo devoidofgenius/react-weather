74edfa75900e0df865e180360b6b61b2bab735e6
devoidofgenius <cellinger@gmail.com>
2017-04-09 17:04:32 -0400

    Add ApiUtils to fetch data needed

---
+++ src/ApiUtils.js
@@ -0,0 +1,29 @@
+const API_KEY = 'e603c81547e24fff301fe9f1ce8aa828';
+
+const openWeatherRequest = (path) => (
+  fetch(`http://api.openweathermap.org/data/2.5${path}`)
+    .then(data => data.json())
+)
+
+const ipLookupRequest = (path) => (
+  fetch('http://ip-api.com/json')
+    .then(data => data.json())
+)
+
+const fetchForecast = ({ weatherData, ipData }) => {
+  const { city, countryCode } = ipData
+  return openWeatherRequest(`/forecast/daily?q=${city},${countryCode}&units=imperial&cnt=3&appid=${API_KEY}`)
+    .then(forecastData => ({ weatherData, ipData, forecastData }))
+}
+
+const fetchWeather = (ipData) => {
+  const { city, countryCode } = ipData
+  return openWeatherRequest(`/weather?q=${city},${countryCode}&units=imperial&appid=${API_KEY}`)
+    .then(weatherData => fetchForecast({ weatherData, ipData }))
+}
+
+export const fetchData = (callback) => {
+  return ipLookupRequest()
+    .then(ipData => fetchWeather(ipData))
+    .then(callback)
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { fetchData } from '../ApiUtils';
 import '../css/App.css'
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: ""
+    };
+  }
+  componentDidMount() {
+    fetchData().then((data) => {
+      this.setState({city: data.ipData.city})
+    });
+  }
+
   render() {
     return (
       <div className="app-wrapper">
-        <h1>Hello, World!</h1>
+        <h1>{this.state.city}</h1>
       </div>
     );
   }
